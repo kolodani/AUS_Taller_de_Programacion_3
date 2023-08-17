57a2599c508fca95d95d421aa00640778a685fbf
Kolodani <danielvinzia@gmail.com>
2023-08-17 16:10:23 -0300

    java, teoria, libro, capitulo 02, ej001, 002

--- 000-libro/002-Todo_es_un_objeto/ej001Cap02/src/App.java
+++ 000-libro/002-Todo_es_un_objeto/ej001Cap02/src/App.java
@@ -3,5 +3,10 @@ public class App {
         System.out.println("Hello, Daniel!");
         String s = new String("asdf");
         System.out.println(s);
+
+        Character ch = 'x';
+        System.out.println(ch);
+        char c = ch;
+        System.out.println(c);
     }
 }
