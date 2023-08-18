b325384b8bac157b1c3f96977991b9b6a0cec072
Kolodani <danielvinzia@gmail.com>
2023-08-18 15:12:10 -0300

    java, teoria, libro, capitulo 02, ejercicio 001

---
+++ 000-libro/002-Todo_es_un_objeto/ejercicio001/src/Ejercicio.java
@@ -0,0 +1,10 @@
+/*
+ *  Ejercicio 1:
+ *  Cree una clase que contenga una variable int y otra char que no estén inicializadas e imprima
+ *  sus valores para verificar que Java se encarga de realizar una inicialización predetenninada.
+*/
+
+public class Ejercicio {
+    int numero;
+    char letra;
+}
---
+++ 000-libro/002-Todo_es_un_objeto/ejercicio001/src/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+    public static void main(String[] args) {
+        Ejercicio a = new Ejercicio();
+        System.out.println("En el caso del numero imprime el valor:");
+        System.out.println(a.numero);
+        System.out.println("En el caso del caracter imprime el valor:");
+        System.out.println(a.letra);
+    }
+}
