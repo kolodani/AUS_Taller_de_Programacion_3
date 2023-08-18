c8b83327a5d2a2eedec44f98bc73b5db74651c87
Kolodani <danielvinzia@gmail.com>
2023-08-18 15:38:16 -0300

    java, teoria, libro, capitulo 02, ejercicio 003

---
+++ 000-libro/002-Todo_es_un_objeto/ejercicio002/ejercicio003/src/ATypeName.java
@@ -0,0 +1,11 @@
+public class ATypeName {
+    private String nombre;
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String n){
+        nombre = n;
+    }
+}
---
+++ 000-libro/002-Todo_es_un_objeto/ejercicio002/ejercicio003/src/App.java
@@ -0,0 +1,12 @@
+/*
+ *  Ejercicio 3:
+ *  Recopile los fragmentos de código relacionados con ATypeNa me y transfónnelos en un programa que
+ *  se pueda compilar y ejecutar.
+*/
+public class App {
+    public static void main(String[] args) {
+        ATypeName lolo = new ATypeName();
+        lolo.setNombre("Grover");
+        System.out.println(lolo.getNombre());
+    }
+}
