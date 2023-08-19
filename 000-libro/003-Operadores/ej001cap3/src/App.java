8fdec7a47a36d70060506d1503f5c789a1783d50
Kolodani <danielvinzia@gmail.com>
2023-08-19 16:17:36 -0300

    java, teoria, libro, capitulo 03, ej001

---
+++ 000-libro/003-Operadores/ej001cap3/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        String a = "Hello, World!";
+        Print impresora = new Print();
+        impresora.print(a);
+    }
+}
