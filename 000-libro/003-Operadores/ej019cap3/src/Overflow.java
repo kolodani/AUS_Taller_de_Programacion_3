23c20200259c3fe2c34fed30ffe82b5f6c652502
Kolodani <danielvinzia@gmail.com>
2023-08-20 20:41:31 -0300

    java, teoria, libro, capitulo 03, ej019

---
+++ 000-libro/003-Operadores/ej019cap3/src/Overflow.java
@@ -0,0 +1,10 @@
+// Sorpresa! Java permite los desbordamientos.
+
+public class Overflow {
+    public static void main(String[] args) {
+        int big = Integer.MAX_VALUE;
+        System.out.println("big = " + big);
+        int bigger = big * 4;
+        System.out.println("bigger = " + bigger);
+    }
+}
