1da3969380793b7f60e683b8d1d26abda4503244
Kolodani <danielvinzia@gmail.com>
2023-08-20 18:44:23 -0300

    java, teoria, libro, capitulo 03, ejercicio 009

---
+++ 000-libro/003-Operadores/ejercicio009/src/App.java
@@ -0,0 +1,14 @@
+/*
+ *  Ejercicio 09:
+ *  Visualice los números más grande y más pequeño que se pueden representar con la notación ex
+ *  ponencial en el tipo float y en el tipo double.
+*/
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("El número más grande que se puede representar con la notación exponencial en el tipo float es: " + Float.MAX_VALUE);
+        System.out.println("El número más pequeño que se puede representar con la notación exponencial en el tipo float es: " + Float.MIN_VALUE);
+        System.out.println("El número más grande que se puede representar con la notación exponencial en el tipo double es: " + Double.MAX_VALUE);
+        System.out.println("El número más pequeño que se puede representar con la notación exponencial en el tipo double es: " + Double.MIN_VALUE);
+    }
+}
