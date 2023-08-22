d2f5086a9fd2d51a2971d56c175d252a1fefa565
Kolodani <danielvinzia@gmail.com>
2023-08-22 12:52:54 -0300

    java, teoria, libro, capitulo 04, ejercicio 001

---
+++ 000-libro/004-Control_de_ejecucion/ejercicio001/src/App.java
@@ -0,0 +1,12 @@
+/*
+ *  Ejercicio 01:
+ *  Escriba un programa que imprima los valores comprendidos entre 1 y 100.
+*/
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        for(int i = 0; i < 100; i++ ){
+            System.out.println( i + 1 );
+        }
+    }
+}
