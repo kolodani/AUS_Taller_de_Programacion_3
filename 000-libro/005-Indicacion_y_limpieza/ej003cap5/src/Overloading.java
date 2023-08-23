6a5c8d2909fc6a7512ab6bfe64f3567a39b2dff3
Kolodani <danielvinzia@gmail.com>
2023-08-23 16:57:49 -0300

    java, teoria, libro, capitulo 05, ej003

---
+++ 000-libro/005-Indicacion_y_limpieza/ej003cap5/src/Overloading.java
@@ -0,0 +1,11 @@
+public class Overloading {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            Tree t = new Tree(i);
+            t.info();
+            t.info("overloaded method");
+        }
+        // Constructor sobrecargado:
+        new Tree();
+    }
+}
