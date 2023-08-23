f6a9d2b27ad8572adc775c22cd91fd04f62f088b
Kolodani <danielvinzia@gmail.com>
2023-08-23 16:57:24 -0300

    java, teoria, libro, capitulo 05, ej001

---
+++ 000-libro/005-Indicacion_y_limpieza/ej001cap5/src/SimpleConstructor.java
@@ -0,0 +1,14 @@
+class Rock{
+    Rock(){
+        // Este es el constructor
+        System.out.print("Rock ");
+    }
+}
+
+public class SimpleConstructor {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Rock();
+        }
+    }
+}
