3344461e30ac67866c09ffe9c1af73298dc305f6
Kolodani <danielvinzia@gmail.com>
2023-08-27 19:52:16 -0300

    java, teoria, libro, capitulo 05, ej025

---
+++ 000-libro/005-Indicacion_y_limpieza/ej025cap5/src/ExplicitStatic.java
@@ -0,0 +1,8 @@
+public class ExplicitStatic {
+    public static void main(String[] args) {
+        System.out.println("Inside main()");
+        Cups.cup1.f(99);
+    }
+    // static Cups Cups1 = new Cups();
+    // static Cups Cups2 = new Cups();
+}
