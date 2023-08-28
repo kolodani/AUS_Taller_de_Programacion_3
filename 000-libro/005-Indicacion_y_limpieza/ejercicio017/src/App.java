392691647e34b7381371b59a4e9ea382bd2c79b1
Kolodani <danielvinzia@gmail.com>
2023-08-28 14:03:03 -0300

    java, teoria, libro, capitulo 05, ejercicio 017

---
+++ 000-libro/005-Indicacion_y_limpieza/ejercicio017/src/App.java
@@ -0,0 +1,14 @@
+/*
+ *  Ejercicio 17:
+ *  Cree una clase con un constructor que tome un argumento String. Durante la construcción, imprima el argumento. Cree
+ *  una matriz de referencias a objetos de esta clase, pero sin crear ningún ob eto para asignarl a la matriz. Cuando
+ *  ejecute el programa, observe si se imprimen los mensajes de inicialización correspondientes a las llamadas al
+ *  constructor.
+*/
+
+public class App {
+    Test[] arreglo1 = new Test[5];
+    public static void main(String[] args) throws Exception {
+        Test[] arreglo2 = new Test[5];
+    }
+}
---
+++ 000-libro/005-Indicacion_y_limpieza/ejercicio017/src/Test.java
@@ -0,0 +1,5 @@
+public class Test {
+    Test(String s){
+        System.out.println("Test Creado: " + s);
+    }
+}
