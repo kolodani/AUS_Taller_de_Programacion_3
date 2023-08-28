f28483d66c2cfcc771496504c5c5acbbf96e7916
Kolodani <danielvinzia@gmail.com>
2023-08-28 14:01:58 -0300

    java, teoria, libro, capitulo 05, ej026

---
+++ 000-libro/005-Indicacion_y_limpieza/ej026cap5/src/Mug.java
@@ -0,0 +1,8 @@
+public class Mug {
+    Mug(int marker){
+        System.out.println("Mug(" + marker + ")");
+    }
+    void f(int marker){
+        System.out.println("f(" + marker + ")");
+    }
+}
---
+++ 000-libro/005-Indicacion_y_limpieza/ej026cap5/src/Mugs.java
@@ -0,0 +1,22 @@
+public class Mugs {
+    Mug mug1;
+    Mug mug2;
+    {
+        mug1 = new Mug(1);
+        mug2 = new Mug(2);
+        System.out.println("mug1 & mug2 initialized");
+    }
+    Mugs(){
+        System.out.println("Mugs()");
+    }
+    Mugs(int i){
+        System.out.println("Mugs(int)");
+    }
+    public static void main(String[] args) {
+        System.out.println("Inside main()");
+        new Mugs();
+        System.out.println("new Mugs() completed");
+        new Mugs();
+        System.out.println("new Mugs(1) completed");
+    }
+}
