499a212f6c242b9b642a6727e19a6dabc26f0cc0
Kolodani <danielvinzia@gmail.com>
2023-08-28 14:02:23 -0300

    java, teoria, libro, capitulo 05, ej029

---
+++ 000-libro/005-Indicacion_y_limpieza/ej029cap5/src/ArraysClassObj.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class ArraysClassObj {
+    public static void main(String[] args) {
+        Random rand = new Random(47);
+        Integer[] a = new Integer[rand.nextInt(20)];
+        System.out.println("length of a = " + a.length);
+        for (int i = 0; i < a.length; i++) {
+            a[i] = rand.nextInt(500);   // conversion automatica
+        }
+        System.out.println(Arrays.toString(a));
+    }
+}
