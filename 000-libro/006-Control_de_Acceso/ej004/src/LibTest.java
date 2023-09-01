53bd4b86f4c473ed216b0725425a34f66f62c98c
Kolodani <danielvinzia@gmail.com>
2023-09-01 17:04:15 -0300

    java, teoria, libro, capitulo 06, ej004

---
+++ 000-libro/006-Control_de_Acceso/ej004/src/LibTest.java
@@ -0,0 +1,9 @@
+//import net.mindview.simple.*;
+import java.util.Vector;
+
+public class LibTest {
+    public static void main(String[] args) {
+        Vector v = new Vector();
+        List l = new List();
+    }
+}
