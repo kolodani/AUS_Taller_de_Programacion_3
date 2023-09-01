ec8d291687c7aef59e45ac0e2216bd3522c92863
Kolodani <danielvinzia@gmail.com>
2023-09-01 17:04:36 -0300

    java, teoria, libro, capitulo 06, ejercicio 001

---
+++ 000-libro/006-Control_de_Acceso/ejercicio001/src/Daniel/App.java
@@ -0,0 +1,12 @@
+/*
+ *  Ejercicio 01:
+ *  Cree una clase dentro de un paquete. Cree una instancia de esa clase fuera de dicho paquete.
+ */
+
+package Daniel;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        new Daniel.Paquete();
+    }
+}
---
+++ 000-libro/006-Control_de_Acceso/ejercicio001/src/Daniel/Paquete.java
@@ -0,0 +1,7 @@
+package Daniel;
+
+public class Paquete {
+    public Paquete() {
+        System.out.println("Instancia de Daniel.Paquete creada.");
+    }
+}
