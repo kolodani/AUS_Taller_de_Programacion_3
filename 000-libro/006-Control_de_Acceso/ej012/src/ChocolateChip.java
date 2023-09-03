ff715742e3c3159ae9ac2134958d64dcb7aca798
Kolodani <danielvinzia@gmail.com>
2023-09-03 20:22:09 -0300

    java, teoria, libro, capitulo 06, ej012

---
+++ 000-libro/006-Control_de_Acceso/ej012/src/ChocolateChip.java
@@ -0,0 +1,12 @@
+public class ChocolateChip extends Cookie {
+    public ChocolateChip(){
+        System.out.println("ChocolateChip constructor");
+    }
+    public void chomp(){
+        bite();
+    }
+    public static void main(String[] args) {
+        ChocolateChip x = new ChocolateChip();
+        x.chomp();
+    }
+}
