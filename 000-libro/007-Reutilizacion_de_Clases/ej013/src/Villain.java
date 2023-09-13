250775246cc87979dfc0e503b2a372ff85f03973
Kolodani <danielvinzia@gmail.com>
2023-09-13 14:42:54 -0300

    java, teoria, libro, capitulo 07, ej013

---
+++ 000-libro/007-Reutilizacion_de_Clases/ej013/src/Villain.java
@@ -0,0 +1,12 @@
+class Villain {
+    private String name;
+    protected void set(String nm) {
+        name = nm;
+    }
+    public Villain(String name) {
+        this.name = name;
+    }
+    public String toString() {
+        return "I'm a Villain and my name is " + name;
+    }
+}
