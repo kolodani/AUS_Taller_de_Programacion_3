22f2c6cf16e205e074dfabe901feb3db5a8a198c
Kolodani <danielvinzia@gmail.com>
2023-09-25 16:51:24 -0300

    java, teoria, libro, capitulo 08, ej002

---
+++ 000-libro/008-Polimorfismo/ej002/src/Music2.java
@@ -0,0 +1,19 @@
+public class Music2 {
+    public static void tune(Wind i) {
+        i.play(Note.MIDDLE_C);
+    }
+    public static void tune(Stringed i) {
+        i.play(Note.MIDDLE_C);
+    }
+    public static void tune(Brass i) {
+        i.play(Note.MIDDLE_C);
+    }
+    public static void main(String[] args) {
+        Wind flute = new Wind();
+        Stringed violin = new Stringed();
+        Brass frenchHorn = new Brass();
+        tune(flute); // Sin generalizacion
+        tune(violin);
+        tune(frenchHorn);
+    }
+}
