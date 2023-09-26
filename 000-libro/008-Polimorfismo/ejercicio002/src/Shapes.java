9d6ab0153dec7b074d7b0e4ccc101f2f292423c6
Kolodani <danielvinzia@gmail.com>
2023-09-26 17:29:58 -0300

    java, teoria, libro, capitulo 08, ejercicio002

---
+++ 000-libro/008-Polimorfismo/ejercicio002/src/Shapes.java
@@ -0,0 +1,17 @@
+/*
+ *  Ejercicio 02:
+ *  Añada la anotación @Override al ejemplo de procesamiento de formas.
+*/
+
+public class Shapes {
+    private static RandomShapeGenerator gen = new RandomShapeGenerator();
+    public static void main(String[] args) {
+        Shape[] s = new Shape[9];
+        // Rellena la matriz con formas:
+        for(int i = 0; i < s.length; i++)
+            s[i] = gen.next();
+        // Realiza llamadas a metodos polimorficos:
+        for(Shape shp : s)
+            shp.draw();
+    }
+}
