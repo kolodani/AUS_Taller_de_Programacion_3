cbcaaa580c9f6995375eaac1cedeea6d243e7a68
Kolodani <danielvinzia@gmail.com>
2023-10-04 14:34:22 -0300

    java, teoria, libro, capitulo 08, ej010

---
+++ 000-libro/008-Polimorfismo/ej010/src/ReferenceCounting.java
@@ -0,0 +1,15 @@
+public class ReferenceCounting {
+    public static void main(String[] args) {
+        Shared shared = new Shared();
+        Composing[] composing = {
+            new Composing(shared),
+            new Composing(shared),
+            new Composing(shared),
+            new Composing(shared),
+            new Composing(shared)
+        };
+        for (Composing c : composing) {
+            c.dispose();
+        }
+    }
+}
