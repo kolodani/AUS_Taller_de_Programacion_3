f32e5f3f70b69898e699064e2216536ff6a80105
Kolodani <danielvinzia@gmail.com>
2023-10-04 14:35:47 -0300

    java, teoria, libro, capitulo 08, ejercicio015

---
+++ 000-libro/008-Polimorfismo/ejercicio015/src/PolyConstructors.java
@@ -0,0 +1,12 @@
+/*
+ *  Ejercicio 15
+ *  Añada una clase RectangularGlyph a PolyCoostructors.java e ilustre el
+ *  problema descrito en esta sección.
+*/
+
+public class PolyConstructors {
+    public static void main(String[] args) {
+        new RoundGlyph(5);
+        new RectangularGlyph(2, 2);
+    }
+}
