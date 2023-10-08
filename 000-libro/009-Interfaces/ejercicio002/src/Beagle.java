74fdf72b6893e47a3ffd018b044a70f38ecd2cca
Kolodani <danielvinzia@gmail.com>
2023-10-08 17:06:17 -0300

    java, teoria, libro, capitulo 09, ejercicio002

---
+++ 000-libro/009-Interfaces/ejercicio002/src/Beagle.java
@@ -0,0 +1,11 @@
+/*
+ *  Ejercicio 02:
+ *  Cree una clase abstracta sin incluir ningún método abstracto y verifique que
+ *  no pueden crearse instancias de esa clase.
+*/
+
+abstract class Beagle {
+    public void bark() {
+        System.out.println("Woof!");
+    }
+}
---
+++ 000-libro/009-Interfaces/ejercicio002/src/Implementacion.java
@@ -0,0 +1,7 @@
+public class Implementacion {
+    public static void main(String[] args) {
+        // Beagle perrito = new Beagle();
+        // no se puede crear una instancia de una clase abstracta
+        // en este caso no se puede instanciar un objeto de la clase Beagle
+    }
+}
