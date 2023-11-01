417d607bcc87fecadba4fe3e37e10cfb261b320e
Daniel Alberto Vinzia <danielvinzia@gmail.com>
2023-11-01 20:17:51 -0300

    cambios hechos por diamand lucho

--- 000-Profesor/010-Socket/src/Sockete.java
+++ 000-Profesor/010-Socket/src/Sockete.java
@@ -3,23 +3,16 @@ import java.io.*;
 
 public class Sockete {
     public static void main(String[] args) {
-        try {
-            ServerSocket ss = new ServerSocket(4567);
-            try {
-                while (true) {
-                    Socket s = ss.accept();
-                    InputStream is = s.getInputStream();
-                    OutputStream os = s.getOutputStream();
-                    os.write("AHHHHHHHHHHHH".getBytes());
-                    s.close();
-                }
-            } catch (IOException e) {
-                System.err.println(e);
+        try (ServerSocket ss = new ServerSocket(4567)) {
+            while (true) {
+                Socket s = ss.accept();
+                InputStream is = s.getInputStream();
+                OutputStream os = s.getOutputStream();
+                os.write("AHHHHHHHHHHHH".getBytes());
+                s.close();
             }
-            ss.close();
         } catch (IOException e) {
             System.err.println("JOE mama");
-            return;
         }
     }
 }
