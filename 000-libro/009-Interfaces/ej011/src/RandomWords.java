84327feff1084e31202834735f1427de5579b876
kolodani <danielvinzia@gmail.com>
2023-11-15 17:03:38 -0300

    java, teoria, libro, capitulo 09, ej011

---
+++ 000-libro/009-Interfaces/ej011/src/RandomWords.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.nio.*;
+
+public class RandomWords implements Readable{
+    private static Random rand = new Random(47);
+    private static final char[] capitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    private static final char[] lowers = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    private static final char[] vowels = "aeiou".toCharArray();
+    private int count;
+    public RandomWords(int count){
+        this.count = count;
+    }
+    public int read(CharBuffer cb) {
+        if (count-- == 0)
+            return -1; // Indica el final de la entrada
+        cb.append(capitals[rand.nextInt(capitals.length)]);
+        for(int i = 0; i < 4; i++) {
+            cb.append(vowels[rand.nextInt(vowels.length)]);
+            cb.append(lowers[rand.nextInt(lowers.length)]);
+        }
+        cb.append(" ");
+        return 10; // Número de caracteres añadidos
+    }
+    public static void main(String[] args) {
+    Scanner s = new Scanner(new RandomWords(10));
+        while(s.hasNext())
+            System.out.println(s.next());
+    }
+}
